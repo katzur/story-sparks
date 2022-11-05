97e782462a587a9a0e5ba1a43a37657189c91268
katzur <zurawska.kasia@gmail.com>
2022-11-05 23:22:49 +0000

    Add product models in products app

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Product, Category, Scent
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ScentAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Scent, ScentAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    """
+    Class for the category model
+    """
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Scent(models.Model):
+    """
+    Class for the scent model
+    """
+
+    name = models.CharField(max_length=256, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    """
+    Class for the product model
+    """
+
+    category = models.ForeignKey(
+        'Category', on_delete=models.SET_NULL, blank=True, null=True)
+    scent = models.ForeignKey(
+        'Scent', on_delete=models.SET_NULL, blank=True, null=True)
+    name = models.CharField(max_length=256, unique=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    details = models.TextField(blank=True, null=True)
+    ingredients = models.CharField(max_length=512, blank=True, null=True)
+    image_url = models.URLField(max_length=1024, blank=True, null=True)
+    image = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
