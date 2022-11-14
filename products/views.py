33061e17eba9c74c6c3a45e3757aadfb243a95dd
katzur <zurawska.kasia@gmail.com>
2022-11-14 20:26:06 +0000

    Add changes to add product view

--- products/views.py
+++ products/views.py
@@ -90,7 +90,7 @@ def add_product(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully added product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(
                 request, 'Failed to add product. Please ensure the form is valid.')
