7bc808cf5984a9b4c322bf4d856172f0d99a4843
katzur <zurawska.kasia@gmail.com>
2022-11-15 19:11:03 +0000

    Add changes to the settings.py

--- story_sparks/settings.py
+++ story_sparks/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f%2x4(9-#$y_j54j9(k!il226-hc_nbe-=+(_x#p5))ytlg%^3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['story-sparks.herokuapp.com', 'localhost']
 
