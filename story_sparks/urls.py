4b685bb9b36e00830e92e10c7744ed2f8fce7c98
katzur <zurawska.kasia@gmail.com>
2022-11-16 21:49:58 +0000

    Add 500 error page

--- story_sparks/urls.py
+++ story_sparks/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler404, handler500
 
 
 urlpatterns = [
@@ -30,3 +30,4 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = 'story_sparks.views.handler404'
+handler500 = 'story_sparks.views.handler500'
--- story_sparks/views.py
+++ story_sparks/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def handler404(request, exception):
     """ Error Handler 404 - Page Not Found """
     return render(request, "errors/404.html", status=404)
+
+
+def handler500(request, *args, **argv):
+    """[Handler for internal server error generic message 500]"""
+    return render(request, "500.html", status=500)
